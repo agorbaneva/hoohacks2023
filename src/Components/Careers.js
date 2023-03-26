9440614bd5cb366cc53f20cd3fbe55fb723145bf
Johnny Pesavento <jpes707@gmail.com>
2023-03-25 22:43:34 -0400

    Job listings

--- src/Components/Careers.js
+++ src/Components/Careers.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../careers.css'
 
 const JobCard = ({ job }) => {
   return (
     <div className="job-card">
-      <h3>{job.job_title}</h3>
-      <p><strong>Employer:</strong> {job.employer_name}</p>
-      {job.employer_logo && <img src={job.employer_logo} alt={`${job.employer_name} logo`} />}
-      <p><strong>Job ID:</strong> {job.job_id}</p>
-      <p><strong>Employment Type:</strong> {job.job_employment_type}</p>
-      <p><strong>Location:</strong> {job.job_city}, {job.job_state}, {job.job_country}</p>
-      <p><strong>Posted At:</strong> {new Date(job.job_posted_at_timestamp * 1000).toLocaleString()}</p>
-      <a href={job.job_apply_link} target="_blank" rel="noreferrer">Apply Now</a>
+      <div className="job-header">
+        <h3 className="job-title">{job.job_title}</h3>
+        <p className="job-location">{job.job_city}, {job.job_state}, {job.job_country}</p>
+      </div>
+      <div className="job-content">
+        <div className="job-left">
+          {job.employer_logo && <img className="job-logo" src={job.employer_logo} alt={`${job.employer_name} logo`} />}
+          <p className="job-employer"><strong>Employer:</strong> {job.employer_name}</p>
+          <p className="job-employment-type"><strong>Employment Type:</strong> {job.job_employment_type}</p>
+          <p className="job-posted-at"><strong>Posted At:</strong> {new Date(job.job_posted_at_timestamp * 1000).toLocaleString()}</p>
+        </div>
+        <div className="job-right">
+        <p className="job-description">{job.job_description.slice(0, 1000)}{job.job_description.length > 1000 ? "..." : ""}</p>
+          <button className="job-apply-btn" onClick={() => window.open(job.job_apply_link, "_blank")}>Apply Now</button>
+        </div>
+      </div>
     </div>
   );
 };
@@ -33,7 +42,7 @@ const Careers = () => {
     axios
       .request(options)
       .then(function (response) {
-        setJobData(response.data);
+        setJobData(response.data.data);
       })
       .catch(function (error) {
         console.error(error);
@@ -42,7 +51,7 @@ const Careers = () => {
 
   return (
     <div className="App">
-      <h1>Job Listings</h1>
+      <h1 className="jobsSectionTitle">Job Listings</h1>
       <div className="job-listings">
         {jobData.map((job) => (
           <JobCard key={job.job_id} job={job} />
